72d6db5f8dcca0cd03a75f5e570caebe0b967938
efuraimujs <efuraimu.js@gmail.com>
2022-08-12 19:37:24 +0300

    milestone-02 2022-08-12T16:37:05+00:00

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 /*
     Print a square with the character #
-    
-    The size of the square must be the first argument 
+    The size of the square must be the first argument
     of the program.
 */
 
@@ -16,8 +15,8 @@ if (process.argv.length <= 2) {
 
 size = parseInt(process.argv[2], 10)
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
+for (var i = 0 ; i < size ; i ++) {
+    for (var j = 0 ; j < size ; j ++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
