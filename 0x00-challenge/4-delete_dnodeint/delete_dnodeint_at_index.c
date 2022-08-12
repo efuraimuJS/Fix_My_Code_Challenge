b9db285a5d858d7ee27faeec136276be28a8f9b0
efuraimujs <efuraimu.js@gmail.com>
2022-08-12 18:55:21 +0300

    milestone-01 2022-08-12T15:24:01+00:00

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,36 +11,43 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *saved_head;
-	dlistint_t *tmp;
-	unsigned int p;
+    dlistint_t *saved_head;
+    dlistint_t *tmp;
+    unsigned int p;
 
-	if (*head == NULL)
-	{
-		return (-1);
-	}
+    if (*head == NULL)
+    {
+        return (-1);
+    }
     saved_head = *head;
-	if (index == 0)
-	{
-		(*head) = saved_head->next;
-        if (saved_head->next)
-            saved_head->next->prev = NULL;
-        saved_head->next = NULL;
-        free(saved_head);
-        return (1);
-	}
-	p = 0;
-    tmp = *head;
-	while (p < index)
-	{
-        if (!tmp)
-            return (-1);
-		tmp = tmp->next;
-		p++;
-	}
-	tmp->prev->next = tmp->next;
-	if (tmp->next)
-		tmp->next->prev = tmp->prev;
-	free(tmp);
-	return (1);
+    p = 0;
+    while (p < index && *head != NULL)
+    {
+        *head = (*head)->next;
+        p++;
+    }
+    if (p != index)
+    {
+        *head = saved_head;
+        return (-1);
+    }
+    if (0 == index)
+    {
+        tmp = (*head)->next;
+        free(*head);
+        *head = tmp;
+        if (tmp != NULL)
+        {
+            tmp->prev = NULL;
+        }
+    }
+    else
+    {
+        (*head)->prev->next = (*head)->next;
+        free(*head);
+        if ((*head)->next)
+            (*head)->next->prev = (*head)->prev;
+        *head = saved_head;
+    }
+    return (1);
 }
--- 0x00-challenge/README.md
+++ 0x00-challenge/README.md
@@ -1,2 +1,3 @@
-# Fix_My_Code_Challenge
-0x00. Fix my code
+# Fix-my-code-0
+
+Fix my code is a new type of project, where we’ll jump into an existing code base and fix it!
